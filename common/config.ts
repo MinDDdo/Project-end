e6ea52ea93c43548b4448e2158224276fb91f89d
Boonraksa Katawundee <myboonraksa12@gmail.com>
2024-01-27 17:50:24 +0700

    เสร็จ

--- common/config.ts
+++ common/config.ts
@@ -4,7 +4,9 @@ dotenv.config({ path: './.env' });
 
 export default  {
     mongodb: {
-        uri: process.env.MONGO_URI
+        uri: process.env.MONGO_URI,
+        username: process.env.MONGO_USERNAME,
+        password: process.env.MONGO_PASSWORD
     },
     jwt: {
         secret_key: process.env.SECRET_KEY
--- database/mongodb.ts
+++ database/mongodb.ts
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 import config from '../common/config';
 
 export const connectMongoDB = async () => {
-    const MONGO_URL = config.mongodb.uri;
-
+    let MONGO_URL = config.mongodb.uri;
+    MONGO_URL = MONGO_URL?.replace("USERNAME", config.mongodb.username || "")
+    MONGO_URL = MONGO_URL?.replace("PASSWORD", config.mongodb.password || "")
+    console.log(MONGO_URL)
     
     try {
         await mongoose.connect(MONGO_URL || '');
